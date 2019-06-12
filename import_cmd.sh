84e0ee40d78cedae799efa2896219bd0e5616269
Zhranklin <chigou79@outlook.com>
2019-06-12 18:55:33 +0800

    Update import_cmd.sh

--- import_cmd.sh
+++ import_cmd.sh
@@ -1,2 +1,2 @@
 alias fortioshow='kubectl port-forward $(kubectl get pod -n $NAMESPACE -l app=fortioclient -o jsonpath='"'{.items[0].metadata.name}'"') 18080:8080 -n $NAMESPACE'
-alias fortiotest="python $FORTIO/runner/runner.py"
+alias fortiotest='python $FORTIO/runner/runner.py'
